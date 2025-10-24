526cda4ec9df9d01b7a7010067834542682c8c57
Finandkz <kanahayaz22@gmail.com>
2025-10-24 22:41:30 +0700

    Pertemuan 4

--- luaslingkaran.php
+++ luaslingkaran.php
@@ -3,9 +3,17 @@
 class luaslingkaran {
     public const phi =  3.14;
     public int $jari;
+    public function tampil($nama = 'roda'){
+         $rumus = luaslingkaran::phi * $lingkaran->jari * $lingkaran->jari;
+         echo "hasilnya adalah: ". $rumus;
+    }
+   public static function testing(){
+    echo"<br>";
+    echo"ini dari static";
+   }
 }
 
-$lingkaran = new luaslingkaran();
-$lingkaran->jari = 10;
-$rumus = luaslingkaran::phi * $lingkaran->jari * $lingkaran->jari;
-echo "hasilnya adalah: ". $rumus;
+$lingkaran = new luaslingkaran(10);
+luaslingkaran::testing()
+
+?>
